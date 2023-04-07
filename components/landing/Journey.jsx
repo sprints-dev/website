d5f1c887bf9b75e87abbedc98b5d022d58b796da
Shayan Ali <64377310+shayanali4@users.noreply.github.com>
2023-04-07 21:02:47 +0500

    carousel added

--- components/landing/Journey.jsx
+++ components/landing/Journey.jsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const Journey = () => {
   return (
@@ -16,7 +18,154 @@ const Journey = () => {
             The When
           </p>
         </div>
-        <div className="grid grid-cols-1 xl:grid-cols-3 items-start grid-rows-[auto__auto__auto__auto__auto__auto] xl:grid-rows-[auto__1fr__auto__1fr] w-full">
+        <div className="w-full journey-slide block xl:hidden">
+          <Splide
+            options={{
+              width: "100%",
+              pagination: true,
+              arrows: true,
+              perPage: 1,
+              perMove: 1,
+              type: "loop",
+              gap: "1rem",
+              drag: true,
+            }}
+          >
+            <SplideSlide>
+              <div className="flex xl:m-0 mb-5 justify-start items-center flex-col w-full gap-5 text-center">
+                <div className="w-full h-auto sm:h-[386px] flex justify-center items-start">
+                  <Image
+                    src={"/dev1.png"}
+                    alt="...dev"
+                    width={386}
+                    height={386}
+                  />
+                </div>
+                <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
+                  Visit our website
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#263254"
+                  className="w-10 h-10 self-center justify-self-center xl:hidden block"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="flex xl:m-0 mb-5 justify-start items-center flex-col w-full gap-5 text-center">
+                <div className="w-full h-auto sm:h-[386px] flex justify-center items-start">
+                  <Image
+                    src={"/dev2.png"}
+                    alt="...dev"
+                    width={220}
+                    height={380}
+                  />
+                </div>
+                <div>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
+                    Book initial call
+                  </p>
+                  <p className="text-blue text-lg -mb-6 sm:text-[1.2rem] font-normal">
+                    Our first meeting
+                  </p>
+                </div>
+                <div className="xl:hidden block self-center justify-self-center">
+                  <a href="#book" className="bookBtn max-w-[200px] ">
+                    Book a call
+                  </a>
+                </div>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
+                <div className="w-full h-auto sm:h-[386px] flex justify-center items-start">
+                  <Image
+                    src={"/dev3.png"}
+                    alt="...dev"
+                    width={386}
+                    height={386}
+                  />
+                </div>
+                <div>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-bold underline">
+                    Define my Product
+                  </p>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-normal">
+                    Your product{"'"}s features
+                  </p>
+                </div>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
+                <div className="w-full h-auto sm:h-[386px] flex justify-center items-start">
+                  <Image
+                    src={"/dev4.png"}
+                    alt="...dev"
+                    width={380}
+                    height={380}
+                  />
+                </div>
+                <div>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
+                    Recieve The Roadmap
+                  </p>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-normal">
+                    Sprints & Budget
+                  </p>
+                </div>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
+                <div className="w-full h-auto sm:h-[386px] flex justify-center items-start">
+                  <Image
+                    src={"/dev5.png"}
+                    alt="...dev"
+                    width={380}
+                    height={380}
+                  />
+                </div>
+                <div>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
+                    Sprints Reviews
+                  </p>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-normal">
+                    Monthly progress
+                  </p>
+                </div>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
+                <div className="w-full h-auto sm:h-[386px] flex justify-center items-start">
+                  <Image
+                    src={"/dev6.png"}
+                    alt="...dev"
+                    width={380}
+                    height={380}
+                  />
+                </div>
+                <div>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
+                    Project delivery
+                  </p>
+                  <p className="text-blue text-lg sm:text-[1.2rem] font-normal">
+                    Ready to Launch
+                  </p>
+                </div>
+              </div>
+            </SplideSlide>
+          </Splide>
+        </div>
+        <div className="hidden xl:grid grid-cols-1 xl:grid-cols-3 items-start grid-rows-[auto__auto__auto__auto__auto__auto] xl:grid-rows-[auto__1fr__auto__1fr] w-full gap-y-7 sm:gap-y-16 gap-x-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -29,12 +178,17 @@ const Journey = () => {
               clipRule="evenodd"
             />
           </svg>
-          <div className="xl:block hidden self-center justify-self-center">
-          </div>
+          <div className="xl:block hidden self-center justify-self-center"></div>
           <div className="xl:block hidden"></div>
           <div className="flex xl:m-0 mb-5 justify-start items-center flex-col w-full gap-5 text-center">
             <div className="w-full h-auto  flex justify-center items-start">
-              <Image style={{ maxWidth: '60%' }} src={"/dev1.png"} alt="...dev" width={1000} height={1000} />
+              <Image
+                style={{ maxWidth: "60%" }}
+                src={"/dev1.png"}
+                alt="...dev"
+                width={1000}
+                height={1000}
+              />
             </div>
             <div>
               <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
@@ -59,7 +213,13 @@ const Journey = () => {
           </div>
           <div className="flex justify-start items-center flex-col w-full gap-10 text-center">
             <div className="w-full h-auto  flex justify-center items-start">
-              <Image style={{ maxWidth: '35%' }} src={"/dev2.png"} alt="...dev" width={1000} height={1000} />
+              <Image
+                style={{ maxWidth: "35%" }}
+                src={"/dev2.png"}
+                alt="...dev"
+                width={1000}
+                height={1000}
+              />
             </div>
             <div>
               <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
@@ -72,7 +232,13 @@ const Journey = () => {
           </div>
           <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
             <div className="w-full h-auto  flex justify-center items-start">
-              <Image style={{ maxWidth: '60%' }} src={"/dev3.png"} alt="...dev" width={1000} height={1000} />
+              <Image
+                style={{ maxWidth: "60%" }}
+                src={"/dev3.png"}
+                alt="...dev"
+                width={1000}
+                height={1000}
+              />
             </div>
             <div>
               <p className="text-blue text-lg sm:text-[1.2rem] font-bold ">
@@ -83,10 +249,19 @@ const Journey = () => {
               </p>
             </div>
           </div>
-          <div style={{ width: '85%', justifySelf: 'center' }} className="bg-[#172651] w-full h-[1px] xl:block hidden mb-[50px] mt-[80px] col-span-3"></div>
+          <div
+            style={{ width: "85%", justifySelf: "center" }}
+            className="bg-[#172651] w-full h-[1px] xl:block hidden mb-[50px] mt-[80px] col-span-3"
+          ></div>
           <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
             <div className="w-full h-auto  flex justify-center items-start">
-              <Image style={{ maxWidth: '60%' }} src={"/dev4.png"} alt="...dev" width={1000} height={1000} />
+              <Image
+                style={{ maxWidth: "60%" }}
+                src={"/dev4.png"}
+                alt="...dev"
+                width={1000}
+                height={1000}
+              />
             </div>
             <div>
               <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
@@ -99,7 +274,13 @@ const Journey = () => {
           </div>
           <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
             <div className="w-full h-auto  flex justify-center items-start">
-              <Image style={{ maxWidth: '60%' }} src={"/dev5.png"} alt="...dev" width={1000} height={1000} />
+              <Image
+                style={{ maxWidth: "60%" }}
+                src={"/dev5.png"}
+                alt="...dev"
+                width={1000}
+                height={1000}
+              />
             </div>
             <div>
               <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
@@ -112,7 +293,13 @@ const Journey = () => {
           </div>
           <div className="flex justify-start items-center flex-col w-full gap-5 text-center">
             <div className="w-full h-auto  flex justify-center items-start">
-              <Image style={{ maxWidth: '60%' }} src={"/dev6.png"} alt="...dev" width={1000} height={1000} />
+              <Image
+                style={{ maxWidth: "60%" }}
+                src={"/dev6.png"}
+                alt="...dev"
+                width={1000}
+                height={1000}
+              />
             </div>
             <div>
               <p className="text-blue text-lg sm:text-[1.2rem] font-bold">
