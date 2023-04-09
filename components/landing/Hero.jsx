7c534eabe8af8c389e1428bb5c24bb8070ef1f8c
nmachado-sprints <nmachado@sprints.dev>
2023-04-09 16:11:14 -0300

    Image background

--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 const Hero = () => {
-  useEffect(()=>{
+  useEffect(() => {
     document.getElementById('hero-video').play();
     document.getElementById('hero-video').removeAttribute("controls");
-  },[]);
+  }, []);
 
   return (
     <div className="wrapper relative isolate z-20">
@@ -13,13 +13,14 @@ const Hero = () => {
         <video
           id="hero-video"
           src="/hero_video.mp4"
-          preload
+          // preload
           autoPlay
           playsInline
           muted
           loop
-          className="w-full h-full absolute left-0 top-0 -z-10 object-cover"
+          className="hidden lg:block w-full h-full absolute left-0 top-0 -z-10 object-cover"
         ></video>
+        <Image src={"/hero-img.jpg"} width={2000} height={2000} className="block lg:hidden w-full h-full absolute left-0 top-0 -z-10 object-cover" />
         <div className="contain h-auto sm:h-screen min-h-[650px] gap-5 py-14 justify-center items-center flex-col text-center">
           <h1 style={styles.textShadowBold} className="text-white text-[35px] sm:text-[40px] lg:text-[50px] leading-[1.2] font-black text-shadow-lg">
             Software MVP Development <br />
